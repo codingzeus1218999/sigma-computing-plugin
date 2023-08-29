80a3237d9f31d5c30b4bfb6c44682cae844aa7ea
Dan Pillay <danpillay87@gmail.com>
2023-08-29 12:32:15 +0100

    Update TagCardPlugin.js

--- src/component/TagCardPlugin.js
+++ src/component/TagCardPlugin.js
@@ -8,7 +8,8 @@ import {
 } from "@sigmacomputing/plugin";
 
 
-import TagCard from "./TagCardExpanded";
+import TagCard from "./TagCard
+";
 
 const emptyValue = [{
   Post_Title: "Generating insights...",
